7311088251bf65939bd695a0a912d99811afe28c
James Fraser <jimbob4t@virginmedia.com>
2020-01-06 09:01:45 +0000

    final commit holiday homework

--- specs/card_game_spec.rb
+++ specs/card_game_spec.rb
@@ -2,17 +2,30 @@ require('minitest/autorun')
 require('minitest/reporters')
 require_relative('../card_game')
 require_relative('../card')
+require( 'pry' )
 Minitest::Reporters.use!
 Minitest::Reporters::SpecReporter.new
 
 class TestCardGame < MiniTest::Test
 
   def setup()
-    @card = Card.new("Hearts", 1)
+    @card1 = Card.new("Hearts", 1)
+    @card2 = Card.new("Clubs", 10)
+    @cards = [@card1, @card2]
   end
 
   def test_check_for_ace()
-    assert_equal(1, @card.value())
+    assert_equal(true, CardGame.check_for_ace(@card1))
   end
+
+  def test_highest_card()
+    assert_equal(@card2, CardGame.highest_card(@card1, @card2))
+  end
+
+  def test_cards_total()
+    assert_equal("You have a total of 11", CardGame.cards_total(@cards))
+  end
+
+
 
 end
